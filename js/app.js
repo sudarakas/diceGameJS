79dc66849d25e1af96c3073f9eafc2dfcfd3aeb0
sudarakas <sudharakafb@gmail.com>
2019-02-07 00:20:39 +0530

    UI updated and basic game logic

---
+++ js/app.js
@@ -0,0 +1,42 @@
+var scores,roundScore,activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('score-2').textContent = '0';
+
+document.getElementById('active-1').textContent = '0';
+document.getElementById('active-2').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //generate a random number between 1 and 6
+    var dice = Math.floor(Math.random()*6) + 1;
+
+    var diceLoc = document.querySelector('.dice');
+    diceLoc.style.display = 'block';
+    diceLoc.src = 'resources/dice-' + dice + '.jpeg';
+
+    if(dice !== 1){
+        roundScore += dice;
+        document.querySelector('#active-' + activePlayer).textContent = roundScore; 
+    }
+    else{
+        activePlayer === 1 ? activePlayer = 2 : activePlayer = 1;
+        roundScore = 0;
+
+        document.querySelector('#active-1').textContent = '0';
+        document.querySelector('#active-2').textContent = '0';
+
+        document.querySelector('.player-1').classList.toggle('active');
+        document.querySelector('.player-2').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+});
+
+document.querySelector('btn-hold').addEventListener('click',function(){
+
+});
